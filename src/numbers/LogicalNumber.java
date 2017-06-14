d16ee92956be1ec83b8748b3bb3049610aca4b19
Navin <Navin@192.168.0.8>
2017-06-14 20:01:40 +0530

    first commmit

---
+++ src/numbers/LogicalNumber.java
@@ -0,0 +1,10 @@
+package numbers;
+
+public class LogicalNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/strings/LogicalString.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class LogicalString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HAI HELLO");
+	}
+
+}
